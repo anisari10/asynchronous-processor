c88a2184fc2ddb85e90136b7611dd952d5cae657
anisari10 <94749249+anisari10@users.noreply.github.com>
2023-07-17 10:38:40 +0530

    Update async_clk.v
    earlier the file had testbench code

--- async_clk.v
+++ async_clk.v
@@ -1,23 +1,11 @@
-module async_clk_tb();
-reg req, ack, sync;
-wire clk;
-
-async_clk async_clk(.req(req), .ack(ack), .clk(clk), .sync(sync));
-initial
+module async_clk(req, ack, clk);
+input wire req, ack;
+output reg clk;
+always@(*)
 begin
-req = 0;
-ack = 0;
-sync = 0;
-
-#10 req = 1;
-#2 req = 0;
-#25 ack = 1;
-#3 ack = 0;
-#40 req = 1;
-#1 req = 0;
-#13 ack = 1;
-#2 ack = 0;
-#37 sync = 1;
-#5 sync = 0;
+if(req==1)
+    clk=1;
+else if(ack==1)
+    clk=0;
 end
 endmodule
